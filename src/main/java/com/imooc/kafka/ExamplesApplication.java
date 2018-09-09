e4dbbdc5434c25a9ca876071abdbec54220d05e6
kingcean <kingcean@126.com>
2018-09-09 20:09:06 +0800

    Kakfa simple example

---
+++ src/main/java/com/imooc/kafka/ExamplesApplication.java
@@ -0,0 +1,14 @@
+package com.imooc.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAutoConfiguration
+@SpringBootApplication
+public class ExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamplesApplication.class, args);
+	}
+}
